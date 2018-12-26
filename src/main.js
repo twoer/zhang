a91af9d1f1a93fea945e1552b788e762a0ecf0f1
张坤 <zhangkun_net@hotmail.com>
2018-12-26 08:43:23 +0800

    1、移除路由

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
 import iView from 'iview'
 import 'iview/dist/styles/iview.css'
 Vue.use(iView)
@@ -9,7 +7,5 @@ Vue.use(iView)
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
   render: h => h(App)
 }).$mount('#app')
